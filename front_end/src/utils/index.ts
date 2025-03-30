d97370b58c31f7ea66622bbb2ae56c88aee81c9d
sharkdingo <1016067756@qq.com>
2025-03-30 21:27:14 +0800

    增加了实时渲染role的功能

--- front_end/src/utils/index.ts
+++ front_end/src/utils/index.ts
@@ -10,3 +10,20 @@ export function parseTime(time: string) {
     let times = time.split(/T|\./)
     return times[0] + " " + times[1]
 }
+
+export function runWithTimeout(
+    task: () => Promise<void>,
+    timeout: number,
+    timeoutError = new Error(`Task timed out after ${timeout}ms`)
+): Promise<void> {
+    let timeoutId: ReturnType<typeof setTimeout>;
+
+    const timeoutPromise = new Promise<never>((_, reject) => {
+        timeoutId = setTimeout(() => reject(timeoutError), timeout);
+    });
+
+    return Promise.race([
+        task().finally(() => clearTimeout(timeoutId)),
+        timeoutPromise
+    ]);
+}
