7597feebbedee20ccf8db2224bf8ba4fc12038fa
231250009-Zhou Xu <231250009@smail.nju.edu.cn>
2025-04-02 23:19:13 +0800

    补充权限逻辑，增加类别条目

--- back_end/src/main/java/com/example/tomatomall/controller/ProductController.java
+++ back_end/src/main/java/com/example/tomatomall/controller/ProductController.java
@@ -1,12 +1,16 @@
 package com.example.tomatomall.controller;
 
+import com.example.tomatomall.exception.TomatoException;
 import com.example.tomatomall.po.Product;
 import com.example.tomatomall.service.ProductService;
+import com.example.tomatomall.util.TokenUtil;
 import com.example.tomatomall.vo.ProductVO;
 import com.example.tomatomall.vo.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -16,11 +20,34 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private TokenUtil tokenUtil;
+
     /**
      * 创建商品
      */
     @PostMapping()
-    public Response<Product> createProduct(@RequestBody ProductVO productVO) {
+    public Response<Product> createProduct(@RequestBody ProductVO productVO, HttpServletRequest request) {
+        String token = null;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("token".equals(cookie.getName())) {
+                    token = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        if (token == null) {
+            throw TomatoException.notLogin();
+        }
+
+        String role = tokenUtil.getUserRoleFromToken(token);
+        if (!"admin".equals(role)) {
+            throw TomatoException.noPermission();
+        }
+
         Product product = productService.createProduct(productVO);
         return Response.buildSuccess(product);
     }
